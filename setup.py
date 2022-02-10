ce4297803e66b1c6de1922ee1fe1204ae5a56c8c
huzongxiang <huzongxiang@yahoo.com>
2022-02-10 16:51:15 +0800

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='crysnet',
-    version='0.2.0',
+    version='0.2.1',
     description='Labelled Graph Networks for machine learning of crystal.',
     long_description=long_description,
     long_description_content_type='text/markdown',
