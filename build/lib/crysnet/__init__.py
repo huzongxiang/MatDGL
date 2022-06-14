b12706d662e5b9a56c5a8e47d49b66667684a453
huzongxiang <huzongxiang@yahoo.com>
2022-06-14 10:00:55 +0800

    MatDGL

--- build/lib/crysnet/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Crystal Neural Network(crysnet)
-Labelled Graph Networks for machine learning of crystal
-Created on Tue Oct 12 20:27:50 2021
-
-@author: huzongxiang
-"""
-__version__ = "0.2.8"
