0b2b4cfc634dc976c7732d51fedc2bddde045ed8
huzongxiang <huzongxiang@yahoo.com>
2022-06-15 10:51:11 +0800

    MatDGL

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='matdgl',
-    version='0.0.1',
+    version='0.0.2',
     description='Graph Neural Networks for machine learning of materials.',
     long_description=long_description,
     long_description_content_type='text/markdown',
